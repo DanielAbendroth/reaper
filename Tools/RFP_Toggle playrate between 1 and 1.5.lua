e4e2ce216fd8589d86b4c04fb88131b393f29ace
ReaperforPodcasting <91094293+ReaperforPodcasting@users.noreply.github.com>
2021-11-06 21:11:57 -0500

    Update RFP_Toggle playrate between 1 and 1.5.lua
    Ensures preserve pitch is always on.

--- Tools/RFP_Toggle playrate between 1 and 1.5.lua
+++ Tools/RFP_Toggle playrate between 1 and 1.5.lua
@@ -1,12 +1,18 @@
 -- @description Toggle playrate between 1 and 1.5
 -- @author Daniel Abendroth (Reaper for Podcasting)
--- @version 1.0
+-- @version 1.1
 -- @about
 --   You can easily toggle your playrate back and forth for faster editing.
 --   You can toggle whether or not you are currently playing audio. It will keep playing if you are currently playing audio but won't start playing if it's paused or stopped.
+-- @changelog
+--   1.1
+--      Now running this script will always have preserve pitch turned on
+--   1.0
+--      Initial release
 
 local rate = reaper.Master_GetPlayRate()
 local playstate = reaper.GetToggleCommandState(1007)
+local pitch = reaper.GetToggleCommandState(40671)
 
 reaper.Undo_BeginBlock()
 if playstate == 1 then
@@ -17,6 +23,11 @@ if rate == 1 then
 	reaper.CSurf_OnPlayRateChange(1.5)
 else
 	reaper.CSurf_OnPlayRateChange(1)
+end
+
+--check and turn on pitch preserve
+if pitch == 0 then
+	reaper.Main_OnCommand(40671, 0)
 end
 
 if playstate == 1 then
