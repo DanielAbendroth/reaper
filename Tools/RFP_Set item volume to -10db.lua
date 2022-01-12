507d2f90fd27261c671e38d1fea2d8d70ee055e6
ReaperforPodcasting <91094293+ReaperforPodcasting@users.noreply.github.com>
2022-01-12 15:18:32 -0600

    Update RFP_Set item volume to -10db.lua
    Better handling for multiple tracks. If an item is selected, the other tracks won't be affected.

--- Tools/RFP_Set item volume to -10db.lua
+++ Tools/RFP_Set item volume to -10db.lua
@@ -1,12 +1,13 @@
 -- @description Set item volume to -10db
 -- @author Daniel Abendroth (Reaper for Podcasting)
--- @version 1.1
+-- @version 1.2
 -- @about
 --   This will set the volume of the audio within the time selection to -10db. This is great for dealing with really loud breaths.
 --   If you don't have any time selection but do have an item selected then it will set that item to -10db.
 --   If you have no time selection or items selected then the script does nothing.
 --   You don't have to stop playback at all. It will catch back up to the play cursor so that you don't interrupt your listening.
 -- @changelog
+--   1.2 Changed how it handles multiple tracks. Now if you have an item selected, it will only reduce the time selection on the selected item. Before, it would reduced the volume on all tracks.  
 --   1.1 A lot of changes to make this work as expected.
 --   1.0 Initial release
 
@@ -31,6 +32,15 @@ if not time_selected and reaper.CountSelectedMediaItems(0) >= 1 then
 	reaper.Undo_BeginBlock()
   count = reaper.CountSelectedMediaItems(0)
   SetVolume(count)
+  action = true
+elseif time_selected and reaper.CountSelectedMediaItems(0) >= 1 then
+  reaper.Undo_BeginBlock()
+  --reaper.Main_OnCommand(40289, 0) --unselect all items
+  reaper.Main_OnCommand(40061, 0) --split items
+  --reaper.Main_OnCommand(40717, 0) --select all items under time selection
+  count = reaper.CountSelectedMediaItems(0)
+  SetVolume(count)
+  reaper.Main_OnCommand(40635, 0) --unselect time selection
   action = true
 elseif time_selected then
 	reaper.Undo_BeginBlock()
